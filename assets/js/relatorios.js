90eebdd55b0aa0f614bcb6f8689698b50fc10a76
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-09-11 07:13:28 -0300

    Adicionando Popup relatorio

--- assets/js/relatorios.js
+++ assets/js/relatorios.js
@@ -1,2 +1,14 @@
 let abrirForm = document.querySelector('.abrirRelatorio');
 let fecharForm = document.querySelector('.closeBtn');
+
+abrirForm.onclick = () => {
+    let relatorio = document.querySelector('.relatorio');
+
+    relatorio.classList.add('show')
+}
+
+fecharForm.onclick = () => {
+    let relatorio = document.querySelector('.relatorio');
+
+    relatorio.classList.remove('show')
+}
