5707423dee320709cdb8da1341305a63fa35c778
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-09-13 20:58:02 -0300

    Criando Historico

---
+++ assets/js/historico.js
@@ -0,0 +1,11 @@
+function abrirForm(){
+    let relatorio = document.querySelector('.relatorio');
+
+    relatorio.classList.add('show');
+}
+
+function fecharForm(){
+    let relatorio = document.querySelector('.relatorio');
+
+    relatorio.classList.remove('show');
+}
