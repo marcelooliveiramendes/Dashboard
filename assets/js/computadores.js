920136003596faf9960734a2a89d552f8ecd0aeb
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-09-14 18:20:54 -0300

    Adicionando botao adicionar

---
+++ assets/js/computadores.js
@@ -0,0 +1,11 @@
+function abrirForm(){
+    let relatorio = document.querySelector('.addComp');
+
+    relatorio.classList.add('showAdd');
+}
+
+function fecharForm(){
+    let relatorio = document.querySelector('.addComp');
+
+    relatorio.classList.remove('showAdd');
+}
