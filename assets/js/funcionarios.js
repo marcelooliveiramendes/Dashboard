d504b819816aa345a29f2bc35c64e0e7045ee6b5
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-09-16 08:38:56 -0300

    Tela Funcionarios

--- assets/js/funcionarios.js
+++ assets/js/funcionarios.js
@@ -8,8 +8,20 @@ function fecharForm(){
     let relatorio = document.querySelector('.editarFunc');
 
     relatorio.classList.remove('showAdd');
+    
 }
 
 function apagarUser(){
     confirm('Tem certeza que deseja apagar o usuário: \n afonsorapadura@gmail.com');
+}
+
+function adicionarFunc(){
+    let addForm = document.querySelector('.addFunc');
+
+    addForm.classList.add('show');
+}
+function fecharAddFunc(){
+    let addForm = document.querySelector('.addFunc');
+
+    addForm.classList.remove('show');
 }
