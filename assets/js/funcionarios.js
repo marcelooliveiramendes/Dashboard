a88813b7437b7fc436d7dea1e1ed016983e6a807
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-09-16 08:41:20 -0300

    Tela Funcionarios

--- assets/js/funcionarios.js
+++ assets/js/funcionarios.js
@@ -1,9 +1,9 @@
+// Abre e fecha o formulario de edição
 function editarForm(){
     let relatorio = document.querySelector('.editarFunc');
 
     relatorio.classList.add('showAdd');
 }
-
 function fecharForm(){
     let relatorio = document.querySelector('.editarFunc');
 
@@ -11,10 +11,12 @@ function fecharForm(){
     
 }
 
+// Mostra a confirmação pra apagar o funcionario
 function apagarUser(){
     confirm('Tem certeza que deseja apagar o usuário: \n afonsorapadura@gmail.com');
 }
 
+// Abre formulario para adicionar um funcionario
 function adicionarFunc(){
     let addForm = document.querySelector('.addFunc');
 
