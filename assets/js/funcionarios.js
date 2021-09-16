ed921518740c5d417ef6aa5c9432ea0af0fdd49d
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-09-16 07:07:14 -0300

    Tela Funcionarios

---
+++ assets/js/funcionarios.js
@@ -0,0 +1,11 @@
+function editarForm(){
+    let relatorio = document.querySelector('.popup');
+
+    relatorio.classList.add('showAdd');
+}
+
+function fecharForm(){
+    let relatorio = document.querySelector('.popup');
+
+    relatorio.classList.remove('showAdd');
+}
